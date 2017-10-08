d11939c9b5bcc637b7395782bd6e8b706fdb76b9
Elisa Vaccaro <erv230@nyu.edu>
2017-10-08 20:28:19 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ emoticons = YAML.load_file('lib/emoticons.yml')
 emoticons
 
 def load_library(file_path)
-answer = {}
+answer = {'get_meaning'=> 0, 'get_emoticon' => 1}
 end
 
 def get_japanese_emoticon
