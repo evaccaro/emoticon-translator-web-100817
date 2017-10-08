cd533df9af418979e94b7829fec594714a63001e
Elisa Vaccaro <erv230@nyu.edu>
2017-10-08 21:00:09 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,12 @@
 require 'yaml'
 
-
-
 def load_library(file_path)
-
   emoticons = YAML.load_file(file_path)
+  emoticons.each do |word, emot|
+    
+  'get_meaning'.keys = emot[1]
+end
+  
   answer = {'get_meaning'=> {}, 'get_emoticon' => {}}
 end
 
